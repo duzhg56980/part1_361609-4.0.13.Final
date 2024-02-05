a8720c9463dcd4df04bd8987b6c6d6bdea1b918d
duzhg56980 <guokey1996@gmail.com>
2024-02-05 17:30:26 +0800

    init commit

---
+++ 361609-4.0.13.Final/FunctionUse140.java
@@ -0,0 +1,6 @@
+import io.netty.handler.stream.ChunkedNioStream;
+public class FunctionUse140 {
+public void funcUse() {
+ChunkedNioStream chunkedniostream = new ChunkedNioStream();
+}
+}
---
+++ 361609-4.0.13.Final/FunctionUse147.java
@@ -0,0 +1,6 @@
+import io.netty.handler.stream.ChunkedStream;
+public class FunctionUse147 {
+public void funcUse() {
+ChunkedStream chunkedstream = new ChunkedStream();
+}
+}
